bbb25d708ce0f2b77c2da2624c086416ef7fc967
EvandroHiga <evandro_higa@hotmail.com>
2024-08-23 14:21:39 -0300

    Initial commit

---
+++ src/main/java/com/higa/AzJvappFromQueueToDbApplication.java
@@ -0,0 +1,13 @@
+package com.higa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzJvappFromQueueToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzJvappFromQueueToDbApplication.class, args);
+	}
+
+}
